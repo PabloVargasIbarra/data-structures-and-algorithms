15d70e91a9f4383d435e7095bd7325cf295c7aea
pablo.vargas <pablovargasibarra12@gmail.com>
2020-06-18 20:50:24 +0200

    Substring equality and longest common using hash functions
    Substring equality and longest common using hash functions

--- data-structures/week3_hash_tables/4_substring_equality/substring_equality.py
+++ data-structures/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -2,15 +2,33 @@
 
 import sys
 
+
 class Solver:
-	def __init__(self, s):
-		self.s = s
-	def ask(self, a, b, l):
-		return s[a:a+l] == s[b:b+l]
+    def __init__(self, s):
+        self.s = s
+        self.m1, self.m2 = 10 ** 9 + 7, 10 ** 9 + 9
+        self.x1, self.x2 = 1, 12  # random
+        self.h1, self.h2 = [0], [0]
+        self.x1_pw = [1]
+        self.x2_pw = [1]
+        for i in range(1, len(self.s)+1):
+            self.x1_pw.append((self.x1_pw[i-1] * self.x1) % self.m1)
+            self.x2_pw.append((self.x2_pw[i - 1] * self.x2) % self.m2)
+        for i in range(1, len(self.s)+1):
+            self.h1.append((self.x1 * self.h1[i - 1] + ord(self.s[i-1])) % self.m1)
+            self.h2.append((self.x2 * self.h2[i - 1] + ord(self.s[i-1])) % self.m2)
+
+    def ask(self, a, b, l):
+        cond1 = (self.h1[a + l] - (self.x1_pw[l] * self.h1[a]) % self.m1) % self.m1 == (
+                    self.h1[b + l] - (self.x1_pw[l] * self.h1[b]) % self.m1) % self.m1
+        cond2 = (self.h2[a + l] - (self.x2_pw[l] * self.h2[a]) % self.m2) % self.m2 == (
+                    self.h2[b + l] - (self.x2_pw[l] % self.m2 * self.h2[b]) % self.m2) % self.m2
+        return cond1 and cond2
+
 
 s = sys.stdin.readline()
 q = int(sys.stdin.readline())
 solver = Solver(s)
 for i in range(q):
-	a, b, l = map(int, sys.stdin.readline().split())
-	print("Yes" if solver.ask(a, b, l) else "No")
+    a, b, l = map(int, sys.stdin.readline().split())
+    print("Yes" if solver.ask(a, b, l) else "No")
--- data-structures/week3_hash_tables/5_longest_common_substring/common_substring.py
+++ data-structures/week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -2,19 +2,107 @@
 
 import sys
 from collections import namedtuple
+import numpy as np
 
 Answer = namedtuple('answer_type', 'i j len')
 
+
+def solve_naive(s, t):
+    ans = Answer(0, 0, 0)
+    for i in range(len(s)):
+        for j in range(len(t)):
+            for l in range(min(len(s) - i, len(t) - j) + 1):
+                if (l > ans.len) and (s[i:i + l] == t[j:j + l]):
+                    ans = Answer(i, j, l)
+    return ans
+
+
+def poly_hash(S, p, x):
+    ans = 0
+    for c in reversed(S):
+        ans = (ans * x + ord(c)) % p
+    return ans
+
+
+def precompute_hashes(text, len_pattern, p, x):
+    H = [None for _ in range(len(text) - len_pattern + 1)]
+    initial_pos_char = len(text) - len_pattern
+    S = text[initial_pos_char:]
+    H[len(text) - len_pattern] = poly_hash(S, p, x)
+    H_set = {H[len(text) - len_pattern]}
+    y = 1
+    for i in range(len_pattern):
+        y = (y * x) % p
+    for i in range(len(text) - len_pattern - 1, -1, -1):
+        H[i] = ((x * H[i + 1]) % p + ord(text[i]) - y * ord(text[i + len_pattern])) % p
+        H_set.add(H[i])
+    return H, H_set
+
+
 def solve(s, t):
-	ans = Answer(0, 0, 0)
-	for i in range(len(s)):
-		for j in range(len(t)):
-			for l in range(min(len(s) - i, len(t) - j) + 1):
-				if (l > ans.len) and (s[i:i+l] == t[j:j+l]):
-					ans = Answer(i, j, l)
-	return ans
+    ans = Answer(0, 0, 0)
+    reverse = False
+    if len(s) > len(t):
+        reverse = True
+        s, t = t, s
+    larger_k = min(len(s), len(t)) + 1
+    lower_k = 0
+    mid_k = (larger_k + lower_k) // 2
+    hash_condition = False
+    p1 = 1000000007
+    p2 = 1000004249
+    x1 = np.random.randint(p1)
+    x2 = np.random.randint(p2)
+    while mid_k > ans.len:
+        if larger_k <= ans.len:
+            return ans
+        Hs_1, Hs_1_set = precompute_hashes(s, mid_k, p1, x1)
+        Ht_1, Ht_1_set = precompute_hashes(t, mid_k, p1, x1)
+        Hs_2, Hs_2_set = precompute_hashes(s, mid_k, p2, x2)
+        Ht_2, Ht_2_set = precompute_hashes(t, mid_k, p2, x2)
+        for i in range(len(Hs_1)):
+            if hash_condition:
+                break
+            if Hs_1[i] in Ht_1_set and Hs_2[i] in Ht_2_set:
+                for j in range(len(Ht_1)):
+                    # print(i,j)
+                    hash_condition = Hs_1[i] == Ht_1[j] and Hs_2[i] == Ht_2[j]
+                    if hash_condition:
+                        # print(True)
+                        if reverse:
+                            ans = Answer(j, i, mid_k)
+                        else:
+                            ans = Answer(i, j, mid_k)
+                        break
+        if hash_condition:
+            lower_k = mid_k
+            hash_condition = False
+        else:
+            larger_k = mid_k
+        mid_k = (larger_k + lower_k) // 2
+    return ans
+
+
+def stress_test():
+    import random
+    n_iter = 10000
+    for _ in range(n_iter):
+        n1 = 100
+        n2 = 20
+        s = ''.join(random.choice('abc') for _ in range(n1))
+        t = ''.join(random.choice('abc') for _ in range(n2))
+        naive = solve_naive(s, t)
+        result = solve(s, t)
+        if result.len != naive.len:
+            print(s, t)
+            print(result)
+            print(naive)
+
 
 for line in sys.stdin.readlines():
-	s, t = line.split()
-	ans = solve(s, t)
-	print(ans.i, ans.j, ans.len)
+    s, t = line.split()
+    ans = solve(s, t)
+    print(ans.i, ans.j, ans.len)
+
+# if __name__ == '__main__':
+#     stress_test()
