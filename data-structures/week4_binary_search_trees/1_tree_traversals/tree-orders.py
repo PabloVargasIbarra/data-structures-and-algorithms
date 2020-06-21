b07d9025c162025b289cbb60f0c964973849f532
pablo.vargas <pablovargasibarra12@gmail.com>
2020-06-21 22:29:10 +0200

    Check if binary search trees structure
    Check if binary search trees structure

--- data-structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ data-structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -1,47 +1,64 @@
 # python3
 
 import sys, threading
-sys.setrecursionlimit(10**6) # max depth of recursion
-threading.stack_size(2**27)  # new thread will get stack of such size
+
+sys.setrecursionlimit(10 ** 6)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+
 
 class TreeOrders:
-  def read(self):
-    self.n = int(sys.stdin.readline())
-    self.key = [0 for i in range(self.n)]
-    self.left = [0 for i in range(self.n)]
-    self.right = [0 for i in range(self.n)]
-    for i in range(self.n):
-      [a, b, c] = map(int, sys.stdin.readline().split())
-      self.key[i] = a
-      self.left[i] = b
-      self.right[i] = c
 
-  def inOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def __init__(self):
+        # read
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for _ in range(self.n)]
+        self.left = [0 for _ in range(self.n)]
+        self.right = [0 for _ in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+        self.in_order = self._in_order(0)
+        self.pre_order = self._pre_order(0)
+        self.post_order = self._post_order(0)
 
-  def preOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def _in_order(self, tree_root):
+        if tree_root == 0:
+            self.result = []
+        if tree_root == -1:
+            return
+        self._in_order(self.left[tree_root])
+        self.result.append(self.key[tree_root])
+        self._in_order(self.right[tree_root])
+        return self.result
 
-  def postOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def _pre_order(self, tree_root):
+        if tree_root == 0:
+            self.result = []
+        if tree_root == -1:
+            return
+        self.result.append(self.key[tree_root])
+        self._pre_order(self.left[tree_root])
+        self._pre_order(self.right[tree_root])
+        return self.result
+
+    def _post_order(self, tree_root):
+        if tree_root == 0:
+            self.result = []
+        if tree_root == -1:
+            return
+        self._post_order(self.left[tree_root])
+        self._post_order(self.right[tree_root])
+        self.result.append(self.key[tree_root])
+        return self.result
+
 
 def main():
-	tree = TreeOrders()
-	tree.read()
-	print(" ".join(str(x) for x in tree.inOrder()))
-	print(" ".join(str(x) for x in tree.preOrder()))
-	print(" ".join(str(x) for x in tree.postOrder()))
+    tree = TreeOrders()
+    print(" ".join(str(x) for x in tree.in_order))
+    print(" ".join(str(x) for x in tree.pre_order))
+    print(" ".join(str(x) for x in tree.post_order))
+
 
 threading.Thread(target=main).start()
--- data-structures/week4_binary_search_trees/2_is_bst/is_bst.py
+++ data-structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -2,22 +2,48 @@
 
 import sys, threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+
+class Tree:
+
+    def __init__(self):
+        # read
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for _ in range(self.n)]
+        self.left = [0 for _ in range(self.n)]
+        self.right = [0 for _ in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+
+    def is_binary_tree(self, tree_root, parent_key, parent_type):
+        if self.n == 0:
+            return True
+        if tree_root == -1:
+            return True
+        if self.left[tree_root] != -1 and self.key[self.left[tree_root]] > self.key[tree_root]:
+            return False
+        if self.right[tree_root] != -1 and self.key[self.right[tree_root]] < self.key[tree_root]:
+            return False
+        if self.right[tree_root] != -1 and parent_type == 'left' and self.key[self.right[tree_root]] > parent_key:
+            return False
+        if self.left[tree_root] != -1 and parent_type == 'right' and self.key[self.left[tree_root]] < parent_key:
+            return False
+        return (self.is_binary_tree(self.left[tree_root], self.key[tree_root], parent_type='left')
+                and self.is_binary_tree(self.right[tree_root], self.key[tree_root], parent_type='right'))
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    tree = Tree()
+    is_binary = tree.is_binary_tree(0, -1, 'none')
+    if is_binary:
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
